b3415f715dc7f4e83ba4fa67227158835b47106f
benkopolis <benkopolis@ce349ab3-abbd-076b-ff20-4646c42d6692>
2010-12-15 09:25:58 +0000

    porzadki w katalogach

--- trunk/ui_contentmanagerwindow.h
+++ trunk/ui_contentmanagerwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'contentmanagerwindow.ui'
 **
-** Created: Wed Dec 15 09:59:35 2010
+** Created: Wed Dec 15 10:25:29 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
--- trunk/ui_filtersdialog.h
+++ trunk/ui_filtersdialog.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'filtersdialog.ui'
 **
-** Created: Mon 29. Nov 07:18:30 2010
-**      by: Qt User Interface Compiler version 4.6.0
+** Created: Wed Dec 15 10:25:29 2010
+**      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
